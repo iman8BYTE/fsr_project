a9351c599cc53dcd9dbc3601a55ab673f78a83f2
iman ben aalla <80457941+iman8BYTE@users.noreply.github.com>
2022-01-28 17:17:56 +0100

    Add files via upload

---
+++ modules.js
@@ -0,0 +1,4 @@
+const bdCours = document.querySelector("modules__cours.bd")
+const bdTd = document.querySelector("modules__td.bd")
+const roCours = document.querySelector("modules__cours.ro")
+const roTd = document.querySelector("modules__td.ro")
